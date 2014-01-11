a77b0b99abbc8b9e725a6cb4df7b2a65f895f63e
Michael Ke Zhang <zikegcwk@gmail.com>
2014-01-10 20:37:34 -0800

    setup links

--- scripts/ng_gallery_app.js
+++ scripts/ng_gallery_app.js
@@ -45,30 +45,13 @@ angular.module('ng-gallery-app').controller('galleryCtrl', function ($scope, $ti
     return $scope.data.currentIndex === index;
   };
 
+  $scope.prevSlide = function () {
+    $scope.data.currentIndex = ($scope.data.currentIndex < $scope.data.slides.length - 1) ? ++$scope.data.currentIndex : 0;
+  };
 
+  $scope.nextSlide = function () {
+    $scope.data.currentIndex = ($scope.data.currentIndex > 0) ? --$scope.data.currentIndex : $scope.data.slides.length - 1;
+  };
 
-  $scope.data.flashTextData = ['Innovation', 'Technology', 'Business', 'Ideas', 'Expertise', 'Opportunity'];
-
-  $scope.data.display_text = $scope.data.flashTextData[0];
-
-//  $scope.data.next_text = function (text_index) {
-//    $timeout(
-//      function(){
-//
-//        text_index = text_index + 1;
-//
-//        if (text_index >= $scope.data.flashTextData.length) {
-//          text_index = 0;
-//        }
-//        $scope.data.display_text = $scope.data.flashTextData[text_index];
-//
-//        $scope.data.next_text(text_index);
-//      },
-//      1500
-//    );
-//  };
-//
-//  var text_index = 0;
-//  $scope.data.next_text(text_index);
 
 });
