906566db601a6de383bf16c5786d3ef38c7a93a0
Michael Ke Zhang <zikegcwk@gmail.com>
2014-01-10 20:08:19 -0800

    adding files

---
+++ scripts/bcg_app.js
@@ -0,0 +1,36 @@
+/**
+ * Created by MKZ on 1/6/14.
+ */
+
+var bcgApp = angular.module('bcgApp',[]);
+
+angular.module('bcgApp').controller('frontPageCtrl', function ($scope, $timeout) {
+  //TODO add any interactions we will have.
+  $scope.data = {
+  };
+
+  $scope.data.flashTextData = ['Innovation', 'Technology', 'Business', 'Ideas', 'Expertise', 'Opportunity'];
+
+  $scope.data.display_text = $scope.data.flashTextData[0];
+
+  $scope.data.next_text = function (text_index) {
+    $timeout(
+      function(){
+
+        text_index = text_index + 1;
+
+        if (text_index >= $scope.data.flashTextData.length) {
+          text_index = 0;
+        }
+        $scope.data.display_text = $scope.data.flashTextData[text_index];
+
+        $scope.data.next_text(text_index);
+      },
+      1500
+    );
+  };
+
+  var text_index = 0;
+  $scope.data.next_text(text_index);
+
+});
