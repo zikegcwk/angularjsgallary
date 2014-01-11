184f0e2046e62da90ddec2cecc65ec88b81f6128
Michael Ke Zhang <zikegcwk@gmail.com>
2014-01-10 20:26:20 -0800

    add images and set up frontend structure

--- scripts/ng_gallery_app.js
+++ scripts/ng_gallery_app.js
@@ -9,6 +9,34 @@ angular.module('ng-gallery-app').controller('galleryCtrl', function ($scope, $ti
   $scope.data = {
   };
 
+  $scope.data.slides = [
+    {
+      'src': 'http://wmphotostudio.smugmug.com/Engagement/KevinCarolyn/i-ssQzkNx/0/X2/K%26C-2-X2.jpg',
+      'description': 'Image 1'
+    },
+    {
+      'src': 'http://wmphotostudio.smugmug.com/Engagement/KevinCarolyn/i-Lg6fSf7/0/X2/K%26C-21-X2.jpg',
+      'description': 'Image 2'
+    },
+    {
+      'src': 'http://wmphotostudio.smugmug.com/Engagement/KevinCarolyn/i-qV3fXqz/0/X3/K%26C-13-X3.jpg',
+      'description': 'Image 3'
+    },
+    {
+      'src': 'http://wmphotostudio.smugmug.com/Engagement/KevinCarolyn/i-mDWdNGH/0/X2/K%26C-18-X2.jpg',
+      'description': 'Image 4'
+    },
+    {
+      'src': 'http://wmphotostudio.smugmug.com/Engagement/KevinCarolyn/i-j2CKL3W/0/X3/K%26C-BEST-3-X3.jpg',
+      'description': 'Image 5'
+    },
+  ];
+
+
+
+
+
+
   $scope.data.flashTextData = ['Innovation', 'Technology', 'Business', 'Ideas', 'Expertise', 'Opportunity'];
 
   $scope.data.display_text = $scope.data.flashTextData[0];
