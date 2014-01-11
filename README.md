81e28f851e7ac32613c60c3f6bd07c4ec2f81be8
Michael Ke Zhang <zikegcwk@gmail.com>
2014-01-10 20:20:45 -0800

    adding files

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ angularjsgallary
 This is a gallery exercise learned from:
 http://onehungrymind.com/build-sweet-photo-slider-angularjs-animate/
 
-Major learning is at ng-animate - other stuff I already know. 
+Major learning is at ng-animate - other stuff I already know.
--- scripts/ng_gallery_app.js
+++ scripts/ng_gallery_app.js
@@ -2,9 +2,9 @@
  * Created by MKZ on 1/6/14.
  */
 
-var bcgApp = angular.module('bcgApp',[]);
+var bcgApp = angular.module('ng-gallery-app',['ngAnimate']);
 
-angular.module('bcgApp').controller('frontPageCtrl', function ($scope, $timeout) {
+angular.module('ng-gallery-app').controller('galleryCtrl', function ($scope, $timeout) {
   //TODO add any interactions we will have.
   $scope.data = {
   };
@@ -13,24 +13,24 @@ angular.module('bcgApp').controller('frontPageCtrl', function ($scope, $timeout)
 
   $scope.data.display_text = $scope.data.flashTextData[0];
 
-  $scope.data.next_text = function (text_index) {
-    $timeout(
-      function(){
-
-        text_index = text_index + 1;
-
-        if (text_index >= $scope.data.flashTextData.length) {
-          text_index = 0;
-        }
-        $scope.data.display_text = $scope.data.flashTextData[text_index];
-
-        $scope.data.next_text(text_index);
-      },
-      1500
-    );
-  };
-
-  var text_index = 0;
-  $scope.data.next_text(text_index);
+//  $scope.data.next_text = function (text_index) {
+//    $timeout(
+//      function(){
+//
+//        text_index = text_index + 1;
+//
+//        if (text_index >= $scope.data.flashTextData.length) {
+//          text_index = 0;
+//        }
+//        $scope.data.display_text = $scope.data.flashTextData[text_index];
+//
+//        $scope.data.next_text(text_index);
+//      },
+//      1500
+//    );
+//  };
+//
+//  var text_index = 0;
+//  $scope.data.next_text(text_index);
 
 });
